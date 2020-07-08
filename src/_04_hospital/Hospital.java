9761daf11eae349d6fe3e6cc3d820e3160e35bbc
phili <phili@LAPTOP-OI44HTNG>
2020-07-07 17:54:45 -0700

    e

--- src/_04_hospital/Hospital.java
+++ src/_04_hospital/Hospital.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Hospital {
+int newdoc=0;
 ArrayList<Doctor> Docs=new ArrayList<Doctor>();
 ArrayList<Patient> Pat=new ArrayList<Patient>();
 
@@ -21,5 +22,17 @@ public void addPatient(Patient patients) {
 }
 public ArrayList getPatients() {
 	return Pat;
+}
+public void assignPatientsToDoctors() throws DoctorFullException {
+	for (int i = 0; i < Pat.size(); i++) {
+
+	try {
+		Docs.get(newdoc).assignPatient(Pat.get(i));
+	
+	} catch (DoctorFullException e) {
+		newdoc+=1;
+		Docs.get(newdoc).assignPatient(Pat.get(i));
+	}
+	}
 }
 }
